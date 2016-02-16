f9c4c7fe8bba7f07e055d9bd111e3ef3ed0ac668
jossmith <joshua.smith@careerbuilder.com>
2016-02-16 13:59:11 -0500

    updates berksfile

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'joshua.smith@careerbuilder.com'
 license 'Apache 2.0'
 description 'Wrapper cookbook to manage Careerbuilder Cassandra nodes on AWS through Opsworks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.1'
+version '0.4.0'
 
 depends 'cassandra-dse'

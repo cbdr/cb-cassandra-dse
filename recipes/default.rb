887aa2e678747cf422ceba809d902ab9ad80f8da
jossmith <jossmith@users.noreply.github.com>
2016-02-17 11:04:07 -0500

    adding in tag code

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,4 @@
+include_recipe "aws"
 include_recipe 'cassandra-dse::default'
 
 directory 'home/ec2-user/.aws' do
@@ -11,4 +12,11 @@ template '/home/ec2-user/.aws/config' do
   group 'ec2-user'  
   mode '0600'
   action :create
+end
+
+unless node['aws-tag']['tags'].empty? || node['aws-tag']['tags'].nil?
+    aws_resource_tag node['ec2']['instance_id'] do
+        tags(node['aws-tag']['tags'])
+        action :update
+    end
 end

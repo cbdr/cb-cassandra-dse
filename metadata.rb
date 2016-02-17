21e929f2bc3a95061357e6669703ed0a1577ac42
jossmith <joshua.smith@careerbuilder.com>
2016-02-17 14:15:27 -0500

    updates aws version needed

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'joshua.smith@careerbuilder.com'
 license 'Apache 2.0'
 description 'Wrapper cookbook to manage Careerbuilder Cassandra nodes on AWS through Opsworks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.4.0'
+version '0.4.1'
 
 depends 'cassandra-dse'
-depends 'aws', '>= 0.2.4'
+depends 'aws', '= 2.5.0'

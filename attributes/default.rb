95ed73075cf5c1312c11b658017894a389245f0c
jossmith <jossmith@users.noreply.github.com>
2016-02-19 13:40:21 -0500

    fixed chef array

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
 override['cassandra']['jmx_server_hostname'] = node['ipaddress']
-override['ntp']['servers'] = [172.21.1.100,172.22.1.100,172.23.1.100,172.24.1.100]
+override['ntp']['servers'] = ['172.21.1.100', '172.22.1.100', '172.23.1.100', '172.24.1.100']

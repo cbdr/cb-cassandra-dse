3bcd7dd6298e45119f49649a27e5f988f31890fc
jossmith <jossmith@users.noreply.github.com>
2016-02-19 14:31:47 -0500

    fixed syntax

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,5 +19,5 @@ unless node['aws-tag']['tags'].empty? || node['aws-tag']['tags'].nil?
     aws_resource_tag node['ec2']['instance_id'] do
         tags(node['aws-tag']['tags'])
         action :update
-    endunco
+    end
 end

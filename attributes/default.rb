4c97ef08570cc080943478e8e961e6235f20cb3b
jossmith <jossmith@users.noreply.github.com>
2016-02-22 12:57:20 -0500

    added new attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
 override['cassandra']['jmx_server_hostname'] = node['ipaddress']
 override['ntp']['servers'] = ['172.21.1.100', '172.22.1.100', '172.23.1.100', '172.24.1.100']
+default['aws-tags']['tags']['Team'] = 'ReplaceMe'
+default['aws-tags']['tags']['Ring'] = 'ReplaceMe'

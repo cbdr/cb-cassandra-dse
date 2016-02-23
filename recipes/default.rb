dd01af2b03fe9ffa4386b19dae4fcc25f7211ba5
jossmith <jossmith@users.noreply.github.com>
2016-02-23 13:05:01 -0500

    added back in ec2-user

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,17 +2,17 @@ include_recipe 'aws'
 include_recipe 'cassandra-dse::default'
 include_recipe 'ntp::default'
 
-#	directory '/home/ec2-user/.aws' do
-#	  mode '0775'
-#	  action :create
-#	end
-#	template '/home/ec2-user/.aws/config' do
-#	  source 'config.erb'
-#	  owner 'ec2-user'
-#	  group 'ec2-user'  
-#	  mode '0600'
-#	  action :create
-#	end
+	directory '/home/ec2-user/.aws' do
+	  mode '0775'
+	  action :create
+	end
+	template '/home/ec2-user/.aws/config' do
+	  source 'config.erb'
+	  owner 'ec2-user'
+	  group 'ec2-user'  
+	  mode '0600'
+	  action :create
+	end
 
 	directory '/root/.aws' do
 	  mode '0775'
@@ -25,6 +25,7 @@ include_recipe 'ntp::default'
 	  mode '0600'
 	  action :create
 	end
+	
 node['aws-tag']['tags'].each do |key,value|
 	execute 'add_tags' do
 		command "aws ec2 create-tags --resources $(curl http://169.254.169.254/latest/meta-data/instance-id) --tags Key=#{key},Value=#{value}"

4691e5845558305d2d4d5da3f3c77a0a1e987ec5
jossmith <jossmith@users.noreply.github.com>
2016-02-23 12:45:19 -0500

    comment out ec2-user

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,17 +2,17 @@ include_recipe 'aws'
 include_recipe 'cassandra-dse::default'
 include_recipe 'ntp::default'
 
-	directory '/home/ec2-user/.aws' do
-	  mode '0775'
-	  action :create
-	end
-	template '/home/ec2-user/.aws/config' do
-	  source 'config.erb'
-	  owner 'ec2-user'
-	  group 'ec2-user'  
-	  mode '0600'
-	  action :create
-	end
+#	directory '/home/ec2-user/.aws' do
+#	  mode '0775'
+#	  action :create
+#	end
+#	template '/home/ec2-user/.aws/config' do
+#	  source 'config.erb'
+#	  owner 'ec2-user'
+#	  group 'ec2-user'  
+#	  mode '0600'
+#	  action :create
+#	end
 
 	directory '/root/.aws' do
 	  mode '0775'

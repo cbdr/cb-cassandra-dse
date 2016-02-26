18fc410c1f17fc722e3e928ca1a2147bca91b568
jossmith <joshua.smith@careerbuilder.com>
2016-02-26 10:18:25 -0500

    adding in cloudwatch monitoring

--- Berksfile
+++ Berksfile
@@ -4,3 +4,5 @@ metadata
 
 cookbook 'cassandra-dse'
 cookbook 'aws'
+cookbook 'yum'
+cookbook 'zip'
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,6 @@
 override['cassandra']['jmx_server_hostname'] = node['ipaddress']
+default['cloudwatch_monitor']['user']              = "ec2-user"
+default['cloudwatch_monitor']['group']             = "ec2-user"
+default['cloudwatch_monitor']['home_dir']          = "/home/ec2-user"
+default['cloudwatch_monitor']['version']           = "1.2.1"
+default['cloudwatch_monitor']['release_url']       = "http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip -O"

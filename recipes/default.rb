5b1515b2f12c95e20946e345d3ce90995b03ee7c
jossmith <joshua.smith@careerbuilder.com>
2016-03-10 09:16:00 -0500

    updated errorcount increment

--- recipes/default.rb
+++ recipes/default.rb
@@ -97,7 +97,7 @@ if node['automated_testing'] == 'true'
 			echo \"$nodes[@]\"
 			errorcount=0
 			if [ ${#nodes[@]} -lt 3 ]; then
-					errorcount++
+					((errorcount+=1))
 					aws ses send-email --from \"automatedtest@cbsitedb.net\" --destination ToAddresses=\"sitedbcloud@careerbuilder.com,logicmonitorsitedb@careerbuildersitedb.pagerduty.com\" --subject \"Cassandra automated testing: Cassandra Failure\" --text \"There is a problem with the Cassandra Opsworks automated tests.One or more nodes has not joined the ring correctly. Please SSH into one of the following nodes 172.21.11.162, 172.21.12.84, or 172.21.13.47.  Contact josh.smith@careerbuilder.com or johnny.thomas@careerbuilder.com for help or information.\"
 			fi
 			ntp=( $(ntpq -p | awk '{print $5}' | grep -Eo '[0-9]') )

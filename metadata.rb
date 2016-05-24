dc3604bfc9b1f696c86855b9ee11ac97b3bb96a6
jossmith <jossmith@users.noreply.github.com>
2016-05-24 12:08:58 -0400

    update version and email

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name 'cb-cassandra-dse'
 maintainer 'Careerbuilder.com'
-maintainer_email 'joshua.smith@careerbuilder.com'
+maintainer_email 'josh.smith@careerbuilder.com'
 license 'Apache 2.0'
 description 'Wrapper cookbook to manage Careerbuilder Cassandra nodes on AWS through Opsworks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.4.2'
+version '0.4.3'
 
 depends 'cassandra-dse'
 depends 'aws'

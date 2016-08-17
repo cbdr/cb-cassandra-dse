6c34e7f59c9325f78690f2bb5276e221af1cf762
John Thomas <jthomas30338@users.noreply.github.com>
2016-08-17 11:27:00 -0400

    Changed maintainer email

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'cb-cassandra-dse'
 maintainer 'Careerbuilder.com'
-maintainer_email 'josh.smith@careerbuilder.com'
+maintainer_email 'johnny.thomas@careerbuilder.com'
 license 'Apache 2.0'
 description 'Wrapper cookbook to manage Careerbuilder Cassandra nodes on AWS through Opsworks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))

7bbc92604fc5aee836ed965475a45705d2d8687a
John Thomas <jthomas30338@users.noreply.github.com>
2016-11-10 11:18:31 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'johnny.thomas@careerbuilder.com'
 license 'Apache 2.0'
 description 'Wrapper cookbook to manage Careerbuilder Cassandra nodes on AWS through Opsworks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.4.3'
+version '0.4.4'
 
 depends 'cassandra-dse'
 depends 'aws'
